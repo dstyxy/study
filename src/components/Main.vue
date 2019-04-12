488484c55e1956fd2edfaf79c2c523767fd49d8d
dstyxy <dstyxy@qq.com>
2019-04-12 14:56:10 +0800

    reset

---
+++ src/components/Main.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="main">
+    
+  </div>
+</template>
+
+<script>
+
+import * as THREE from 'three'
+
+export default {
+  name: 'Main',
+  props: {
+    
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods:{
+    createBlock() {
+      var camera, scene, renderer;
+      var geometry, material, mesh;
+
+      init();
+      animate();
+
+      function init() {
+
+        camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        camera.position.z = 1;
+
+        scene = new THREE.Scene();
+
+        geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        material = new THREE.MeshNormalMaterial();
+
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+
+      }
+
+      function animate() {
+
+        requestAnimationFrame( animate );
+
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.02;
+
+        renderer.render( scene, camera );
+
+      }
+    }
+  },
+  mounted() {
+    this.createBlock()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
